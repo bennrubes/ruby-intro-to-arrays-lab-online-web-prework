bc763109fc43a8345d17c6a58b19d738ac4887b4
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-30 16:03:33 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,6 +2,6 @@ def instantiate_new_array
   my_new_array = []
 end
 
-def array_with_two_elements(element1, element2)
-  ["#{element1}", "#{element2}"]
+def array_with_two_elements
+  two_elemnts = [1, 2]
 end
